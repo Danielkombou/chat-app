e8d8e7465cfa1a570accaf5b955a0bba7482c2da
Danielkombou <danielkombou7@gmail.com>
2024-06-10 18:24:48 +0100

    server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Routing from "./components/Routes";
 // import Register from "./components/Register/Register";
 
 function App() {
-  axios.defaults.baseURL = "http://localhost:4040";
+  axios.defaults.baseURL = "https://chat-app-dan.vercel.app";
   axios.defaults.withCredentials = true;
 
   return (
