162ae384cbcf91e4b37a52495c9cd31ab5437de3
Danielkombou <danielkombou7@gmail.com>
2024-06-29 13:25:07 +0100

    managing the chat section

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -337,7 +337,10 @@ export default function Chat() {
                 className="absolute top-2 left-2 md:hidden bg-gray-200 p-1 rounded z-10"
                 onClick={() => setSelectedUserId(false)}
               >
-                Back
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+  <path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 0 1 0 1.06l-6.22 6.22H21a.75.75 0 0 1 0 1.5H4.81l6.22 6.22a.75.75 0 1 1-1.06 1.06l-7.5-7.5a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
+</svg>
+
               </button>
               <div className="overflow-y-scroll absolute top-0 right-0 left-0 bottom-2 w-full">
                 {messagesWithoutDupes.map((message) => (
